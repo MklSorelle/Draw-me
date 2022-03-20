f4e38ba220ead60ee2d0cdd4d1fad9ef14a74bbf
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2022-03-20 23:27:22 +0100

    premier commit

---
+++ partials/header_dessin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dessin_Style.css">
+    <link href="http://fonts.cdnfonts.com/css/pumpkin-cheesecake" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/caveat-brush" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/cabinsketch" rel="stylesheet">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
+
+    <script src="./html2canvas.js"></script>
+
+    <script src="./index.js" defer></script>
+    <script src="https://kit.fontawesome.com/426caaf4c5.js" crossorigin="anonymous"></script>
+    
+    <title>Draw me</title>
+</head>
+
+<body>
+    
---
+++ partials/header_intro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./acceuil_style.css">
+    <link href="http://fonts.cdnfonts.com/css/cabinsketch" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/caveat-brush" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/pumpkin-cheesecake" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/426caaf4c5.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/0c87a70838.js"></script>
+    <title>Draw me</title>
+</head>
+
+<body>
